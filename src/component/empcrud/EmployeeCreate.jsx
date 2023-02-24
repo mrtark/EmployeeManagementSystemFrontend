ec83178c0ca508bf7ab505294c7257226b196a34
mrtark <murat_ark97@hotmail.com>
2023-02-24 17:03:41 +0300

    EmployeeCreate page

--- src/component/empcrud/EmployeeCreate.jsx
+++ src/component/empcrud/EmployeeCreate.jsx
@@ -1,10 +1,55 @@
 import React, { Component } from 'react'
+import EmployeeService from '../../service/EmployeeService';
 
 export default class EmployeeCreate extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: this.props.match.params.id,
+      department: "",
+      gender: "",
+      username: "",
+      surname: "",
+      telephoneNumber: "",
+      emailAdress: "",
+    }
+  }
+
+  componentDidMount() {
+    if (this.state.id === "/employee-create") {
+      return;
+    } else {
+      EmployeeService.employeeFindApi(this.state.id).then(
+        getData => {
+          const employeeDto = getData.data;
+          console.log("Update detData: " + employeeDto)
+          this.setState({
+            department: employeeDto.department,
+            gender: employeeDto.gender,
+            username: employeeDto.username,
+            surname: employeeDto.surname,
+            telephoneNumber: employeeDto.telephoneNumber,
+            emailAdress: employeeDto.emailAdress,
+            department: employeeDto
+          })
+        }
+      )
+    }
+  }
+
+  dynamicHeaderCU() {
+    if (this.state.id === "/employee-create") {
+      return <h1>Employe Create</h1>
+    } else {
+      return <h1>Employe Update</h1>
+    }
+  }
+
   render() {
     return (
       <>
-      EmployeeCreatePage
+        {this.dynamicHeaderCU()}
       </>
     )
   }
